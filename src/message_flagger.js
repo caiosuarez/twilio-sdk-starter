01c31ac4f0460cf6c9c0429206320f16b71905e0
Caio <caiosasia@gmail.com>
2023-11-24 16:33:15 -0300

    first commit

---
+++ src/message_flagger.js
@@ -0,0 +1,25 @@
+const Twilio = require("twilio");
+
+// const config = require("./config");
+
+function messageFlagger(e) {
+  console.log("e", e);
+
+  // if (config.TWILIO_SYNC_SERVICE_SID) {
+  //   // Point to a particular Sync service, or use the account default to
+  //   // interact directly with Functions.
+  //   const syncGrant = new SyncGrant({
+  //     serviceSid: config.TWILIO_SYNC_SERVICE_SID || "default",
+  //   });
+  //   token.addGrant(syncGrant);
+  // }
+
+  // Serialize the token to a JWT string and include it in a JSON response
+  //   return {
+  //     identity: token.identity,
+  //     token: token.toJwt(),
+  //   };
+  return null;
+}
+
+module.exports = messageFlagger;
