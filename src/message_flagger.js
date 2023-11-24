4b1207d4f09f7cc4035835d10f5a5f822dbab2db
Caio <caiosasia@gmail.com>
2023-11-24 17:15:33 -0300

    code fix

--- src/message_flagger.js
+++ src/message_flagger.js
@@ -3,14 +3,12 @@ const Twilio = require("twilio");
 // const config = require("./config");
 
 function messageFlagger(req, res) {
-  console.log("req", req);
-  console.log("res", res);
   const body = req.body;
   const messageText = body["Body"];
   const author = body["Author"];
   const attributes = body["Attributes"] || {};
 
-  if (author === 2) {
+  if (author === "2") {
     res.status(200).send({});
     return;
   }
