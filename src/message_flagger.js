2dda55c57626e99f78dc09efe2a230c3572551d2
Caio <caiosasia@gmail.com>
2023-11-24 17:10:52 -0300

    test flag message from backend

--- src/message_flagger.js
+++ src/message_flagger.js
@@ -5,22 +5,21 @@ const Twilio = require("twilio");
 function messageFlagger(req, res) {
   console.log("req", req);
   console.log("res", res);
+  const body = req.body;
+  const messageText = body["Body"];
+  const author = body["Author"];
+  const attributes = body["Attributes"] || {};
 
-  // if (config.TWILIO_SYNC_SERVICE_SID) {
-  //   // Point to a particular Sync service, or use the account default to
-  //   // interact directly with Functions.
-  //   const syncGrant = new SyncGrant({
-  //     serviceSid: config.TWILIO_SYNC_SERVICE_SID || "default",
-  //   });
-  //   token.addGrant(syncGrant);
-  // }
+  if (author === 2) {
+    res.status(200).send({});
+    return;
+  }
 
-  // Serialize the token to a JWT string and include it in a JSON response
-  //   return {
-  //     identity: token.identity,
-  //     token: token.toJwt(),
-  //   };
-  return null;
+  if (messageText.includes("nervous")) {
+    attributes["flagged"] = true;
+  }
+  res.status(200).send({ attributes: attributes });
+  return;
 }
 
 module.exports = messageFlagger;
