60d195a22c886fc54b31b3c5e4d5608612357bfd
Caio <caiosasia@gmail.com>
2023-11-27 14:37:04 -0300

    fix message attribute update

--- src/message_flagger.js
+++ src/message_flagger.js
@@ -1,4 +1,3 @@
-const Twilio = require("twilio");
 const config = require("./config");
 const accountSid = config.TWILIO_ACCOUNT_SID;
 const authToken = config.TWILIO_AUTH_TOKEN;
@@ -24,43 +23,17 @@ async function messageFlagger(req, res) {
     return;
   }
 
-  console.log("chatServiceSid", chatServiceSid);
-  console.log("conversationSid", conversationSid);
-  console.log("messageSid", messageSid);
-
   attributes["flagged"] = true;
-  console.log("attributes", attributes);
   try {
     client.conversations.v1
       .services(chatServiceSid)
       .conversations(conversationSid)
       .messages(messageSid)
-      .update({ attributes: attributes })
-      .then((message) => console.log("message", message));
+      .update({ attributes: JSON.stringify(attributes) })
+      .then((message) => console.log("message updated", message));
   } catch (err) {
     console.log("err", err);
   }
-
-  try {
-    client.conversations.v1
-      .services("IS61925b5a480e2574976fea202ebd3c5e")
-      .conversations(conversationSid)
-      .messages(messageSid)
-      .update({ attributes: attributes })
-      .then((message) => console.log("message", message));
-  } catch (err) {
-    console.log("err 2", err);
-  }
-
-  client.conversations.v1
-    .services(chatServiceSid)
-    .conversations(conversationSid)
-    .messages.list({ limit: 20 })
-    .then((messages) => messages.forEach((m) => console.log(m.sid)));
-
-  //   res.status(200).send({ attributes: attributes });
-  //   res.status(200).send(JSON.stringify({ attributes: attributes }));
-  //   return;
 }
 
 module.exports = messageFlagger;
